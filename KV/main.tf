be9f1ad8009dcff6996b0026d4e7f7ac7f64dbc1
Saravanagopi <ssgopi1983@yahoo.co.in>
2023-06-08 15:21:27 -0400

    Update main.tf

--- KV/main.tf
+++ KV/main.tf
@@ -12,7 +12,6 @@ resource "azurerm_key_vault" "keyvault" {
   enable_rbac_authorization   =  true
   enabled_for_disk_encryption = false
   tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_enabled         = true
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
 
